720257d5b88526ddb8f197da287a734f3fae95f7
Ben <benjamin.s.record@gmail.com>
2022-12-01 13:10:33 -0700

    Patch: dmenu-alpha-20210605-1a13d04.diff

--- config.def.h
+++ config.def.h
@@ -4,6 +4,7 @@
 // clang-format off
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xf0;
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 400;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -17,6 +18,13 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#f8f8f2", "#6272a4" },
  	[SchemeOut] = { "#000000", "#00ffff" },
 };
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 25;
 
